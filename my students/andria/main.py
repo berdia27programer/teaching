213912362af6b3a268ddeddfe61897a92e009856
berdia27programmer <berdiabekauri5@gmail.com>
2026-02-08 16:56:44 -0800

    commit

--- my students/andria/main.py
+++ my students/andria/main.py
@@ -1,2 +1,28 @@
-print("berdia")
-print("andria")
+# print("berdia")
+# print("andria")
+
+# 1. ცვლადის სახელი
+# 2. =
+# 3. ცვლადის მნიშვნელობა
+
+# name = "berdia"
+
+# print("name")
+# print(name)
+
+# string = "andria"
+# integer = 7
+# Float = 2.5
+
+# print(string)
+# print(integer)
+# print(Float)
+
+# name = "berdia" # ცვლადი
+# name("berdia") # ფუნქცია
+
+# name = "berdia"
+# name = 5
+# name = 2.5
+
+# print(name)
--- my students/giorgi/main.py
+++ my students/giorgi/main.py
@@ -1,2 +1,31 @@
 # print("berdia")
 # print("giorgi")
+
+# name = "giorgi"
+
+# print(name)
+
+# name = "giorgi"
+
+# print(name)
+
+
+# name = "berdia"
+
+# print(name)
+
+# string1 = "asdasd"
+# string2 = "asdasdasd"
+# string3 = "asdasd"
+# string4 = "asdasd"
+# string5 = "asdasd"
+# int1 = 1
+# int2 = 2
+# int3 = 3
+# int4 = 4
+# int5 = 5
+# float1 = 1.5
+# float2 = 2.5
+# float3 = 3.5
+# float4 = 4.5
+# float5 = 5.5

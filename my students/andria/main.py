ab2f84d19450b55c4604d8a359b0ea0f3e5395a8
berdia27programmer <berdiabekauri5@gmail.com>
2026-02-12 19:00:26 -0800

    commit

--- my students/andria/main.py
+++ my students/andria/main.py
@@ -26,3 +26,48 @@
 # name = 2.5
 
 # print(name)
+
+# string1 = "berdia"
+# string2 = "andria"
+# string3 = "beka"
+# string4 = "asdasdasd"
+# string5 = "asdasdasd"
+# int1 = 1
+# int2 = 2
+# int3 = 3
+# int4 = 4
+# int5 = 5
+# float1 = 1.5
+# float2 = 2.5
+# float3 = 3.5
+# float4 = 3.5
+# float5 = 5.5
+
+# print(string1)
+# print(string2)
+# print(string3)
+# print(string4)
+# print(string5)
+# print(int1)
+# print(int2)
+# print(int3)
+# print(int4)
+# print(int5)
+# print(float1)
+# print(float2)
+# print(float3)
+# print(float4)
+# print(float5)
+
+# print(float5)
+# print("float5")
+
+# x = 2
+# y = 4
+
+# print(x + y)
+# print(x - y)
+# print(x * y)
+# print(x / y) # float: 2.5
+# print(x // y) # int: 2
+# print(x % y) # 0

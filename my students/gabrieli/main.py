18d223b22e188d0be6914c8d5f2a452644a39cb8
berdia27programmer <berdiabekauri5@gmail.com>
2026-03-13 17:51:49 -0700

    commit

--- my students/gabrieli/main.py
+++ my students/gabrieli/main.py
@@ -412,3 +412,12 @@
 #     print("there is no index")
 
 # print(array[num - 1])
+
+# indexes
+#      0   1  2  3  4  5  6  7  8  9
+# nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# #     -10 -9 -8 -7 -6 -5 -4 -3 -2 -1
+
+# print(nums[1]) # 2
+# print(nums[-1]) # 10
+# print(nums[-9])

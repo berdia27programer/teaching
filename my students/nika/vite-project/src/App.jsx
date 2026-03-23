c8ffb000abe834f1317ddd1f84df7eb599e81f59
berdia27programmer <berdiabekauri5@gmail.com>
2026-03-22 18:20:49 -0700

    commit

--- my students/nika/vite-project/src/App.jsx
+++ my students/nika/vite-project/src/App.jsx
@@ -1,10 +1,25 @@
-import Button from "./components/Button";
+import { useState } from "react";
+// import Button from "./components/Button";
 
 export default function App() {
+  const [count, setCount] = useState(0)
+
+  const add = () => {
+    setCount(count + 1) 
+  }
+
+  const subtract = () => {
+    setCount(count - 1)
+  }
+
   return (
     <>
-      <h1>Hello</h1>
-      <Button />
+      <p>{count}</p>
+      <button onClick={add}>+</button>
+      <button onClick={subtract}>-</button>
+
+      {/* <h1>Hello</h1>
+      <Button /> */}
     </>
   )
 }

6bee90243cef5b6f63706ed304fb61d348070bfa
berdia27programmer <berdiabekauri5@gmail.com>
2026-04-03 20:19:35 -0700

    commit

--- my students/davit/main.py
+++ my students/davit/main.py
@@ -1,3 +1,20 @@
 # print("hello " + 1)
 
 # No classwork
+
+# string - ბრჭყალებში ჩასმულ ტექსტს - "hello"/'hello'
+# integer - რიცხვი - 123
+# float - ათწილადი - 2.5
+
+# print(2 + 4)
+# print(2 - 4)
+# print(2 * 4)
+# print(2 / 4) # float
+# print(2 // 4) # integer
+
+#სახ.   მნიშვნ.
+# name1 = "berdia"
+# name2 = "dato"
+
+# print(name1)
+# print(name2)

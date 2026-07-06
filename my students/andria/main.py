7b99535010a9e7900f58f1850c38edd38622f55d
berdia27programmer <berdiabekauri5@gmail.com>
2026-07-06 01:26:50 -0700

    commit

--- my students/andria/main.py
+++ my students/andria/main.py
@@ -169,3 +169,16 @@
 # forward(200)
 
 # exitonclick()
+
+#         0   1  2  3  4  5  6  7  8  9
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# print(numbers[4])
+
+# print(numbers[0:5])
+
+# def greet(name):
+#     print("Hello", name)
+
+# greet("world")
+# greet("Berdia")

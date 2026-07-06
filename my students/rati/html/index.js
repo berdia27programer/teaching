efca20a5e3cf72f4b048c999b78aa368feeb0c0f
berdia27programmer <berdiabekauri5@gmail.com>
2026-07-06 05:26:22 -0700

    commit

---
+++ my students/rati/html/index.js
@@ -0,0 +1,34 @@
+// const name = "berdia"
+// let age = 13
+
+// console.log(name)
+// console.log(age)
+
+// Arrow func
+
+// const greet = () => {
+//     console.log("Hello world")
+// }
+
+// // Func
+
+// function greet2() {
+//     console.log("Hello world")
+// }
+
+// setTimeout(() => {
+//     console.log("Hello world")
+// }, 5000)
+
+// setInterval(() => {
+//     console.log("Goodbye")
+// }, 5000)
+
+// if (5 > 3) {
+//     console.log(true)
+// } else if (5 == 3) {
+//     console.log("what")
+// } else {
+//     console.log(false)
+// }
+

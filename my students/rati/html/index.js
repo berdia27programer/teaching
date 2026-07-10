4421457814b9d5af547d6555159d944797e21ab0
berdia27programmer <berdiabekauri5@gmail.com>
2026-07-10 13:11:33 -0700

    commit

--- my students/rati/html/index.js
+++ my students/rati/html/index.js
@@ -32,26 +32,58 @@
 //     console.log(false)
 // }
 
-const btn = document.getElementById("btn")
+// const btn = document.getElementById("btn")
 
-const data = []
+// const data = []
 
-const signup = e => {
-    e.preventDefault()
+// const signup = e => {
+//     e.preventDefault()
 
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+//     const username = document.getElementById("username").value;
+//     const password = document.getElementById("password").value;
 
-    if (username === "" || password === "") {
-        alert("Enter all inputs")
+//     if (username === "" || password === "") {
+//         alert("Enter all inputs")
+//     }
+
+//     data.push({
+//         Username: username,
+//         Password: password
+//     })
+
+//     localStorage.setItem(database, data) // const database = data
+// }
+
+// btn.onclick = signup
+
+// addEventListener("click", (e) => {
+//     console.log(e);
+// })
+
+// const numbers = [1, 2, 3, 4, 5]
+
+// numbers.map(i, () => {
+//     console.log(`This number is ${i}`)
+//     // This is number 1
+//     // This is number 2
+//     // ...
+// })
+
+// numbers.forEach(i, () => {
+//     if (i % 2 === 0) {
+//         console.log("even")
+//     } else {
+//         console.log("odd")
+//     }
+// })
+
+class Person {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
     }
-
-    data.push({
-        Username: username,
-        Password: password
-    })
-
-    localStorage.setItem(database, data) // const database = data
 }
 
-btn.onclick = signup
+const person = new Person("berdia", 13)
+
+console.log(person)
---
+++ my students/todua/html/index.js
@@ -0,0 +1,16 @@
+// class Person {
+//     constructor(name, age) {
+//         let.name = name
+//         let.age = age
+//     }
+// }
+
+// const numbersSet = new Set([1, 2, 3])
+
+// numbersSet.add(4)
+
+// numbersSet.delete(3)
+
+// numbersSet.clear()
+
+// console.log(numbersSet.has(2))

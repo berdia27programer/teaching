952576579d7f1639c83de3f4f275ac700bac6598
berdia27programmer <berdiabekauri5@gmail.com>
2026-08-19 05:28:32 -0700

    commit

---
+++ my students/gabriel/html/index.js
@@ -0,0 +1,11 @@
+// class Person {
+//     constructor(name, lastName, age) {
+//         this.name = name
+//         this.lastName = lastName
+//         this.age = age
+//     }
+// }
+
+// const person = new Person("berdia", "bekauri", 13)
+
+// console.log(person)
---
+++ my students/maxaradze/main.py
@@ -0,0 +1,8 @@
+# # for loop
+
+# for i in range(1, 51):
+#     print(i)
+
+# # while loop
+# while True:
+#     print("hello")
